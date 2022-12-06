50b2210c1d16f736f7b571a83d065472e12b740c
Marc <mgm793@gmail.com>
2022-12-06 09:26:30 +0100

    6th challenge

--- 6.js
+++ 6.js
@@ -6,26 +6,30 @@ const input = fs.readFileSync(filename, "utf-8");
 let start = 0;
 const visited = {};
 for (let i = 0; i < input.length; i++) {
-  if (visited[input[i]] !== undefined && i - visited[input[i]] < 4) {
-    start = visited[input[i]] + 1;
-  }
-  if (i - start === 4) {
+  if (i - start === 4){
     console.log(i);
     break;
+  }
+  if (visited[input[i]] !== undefined && ((i - visited[input[i]]) < 4)) {
+    start = visited[input[i]] + 1 <= start ? start : visited[input[i]] + 1;
   }
   visited[input[i]] = i;
 }
-//second
 
+
+//second
 let start2 = 0;
 const visited2 = {};
 for (let i = 0; i < input.length; i++) {
-  if (visited2[input[i]] !== undefined && i - visited2[input[i]] < 14) {
-    start2 = visited2[input[i]] + 1;
-  }
-  if (i - start2 === 14) {
+  if (i - start2 === 14){
     console.log(i);
     break;
   }
+  if (visited2[input[i]] !== undefined && ((i - visited2[input[i]]) < 14)) {
+    start2 = visited2[input[i]] + 1 <= start2 ? start2 : visited2[input[i]] + 1;
+  }
   visited2[input[i]] = i;
 }
+
+//bvwbjplbgvbhsrlpgdmjqwftvncz
+//0123456789012345678901234567
